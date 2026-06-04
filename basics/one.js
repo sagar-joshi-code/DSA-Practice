cf47a9f8145e6e8b024f8e9ef7c963214bc7e016
sagar joshi <sagarjo637@gmail.com>
2026-06-04 18:13:21 +0545

    added index.html

---
+++ basics/one.js
@@ -0,0 +1,23 @@
+//sum of two integer
+
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+//Realation between integer and string
+let c = 12;
+let d = "13";
+console.log(c + d); //concatinate
+
+let e = 12;
+let f = "13";
+console.log(e - f); //here concatinate does not work between string and number and if we use minus(-) sign javascript automatically convert string into number
+
+//sum and message
+let g = 15;
+let h = 15;
+console.log(g+h + " is sum of 15 and 15  " );
+
+//accept and print the number
+let age = prompt("Enter your age");
+console.log(age);
