42436552f11a45b27128ed57497c1eea1d0f8357
sagar joshi <sagarjo637@gmail.com>
2026-06-04 18:46:38 +0545

    practice swapping variable from first method

--- basics/one.js
+++ basics/one.js
@@ -1,23 +1,35 @@
-//sum of two integer
+// //sum of two integer
 
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+
+// //Realation between integer and string
+// let c = 12;
+// let d = "13";
+// console.log(c + d); //concatinate
+
+// let e = 12;
+// let f = "13";
+// console.log(e - f); //here concatinate does not work between string and number and if we use minus(-) sign javascript automatically convert string into number
+
+// //sum and message
+// let g = 15;
+// let h = 15;
+// console.log(g + h + " is sum of 15 and 15  ");
+
+// //accept and print the number
+// let age = Number(prompt("Enter your age"));
+// console.log(age);
+
+//swap two variable via 3 method
+
+//first method
 let a = 10;
 let b = 20;
-console.log(a + b);
-
-//Realation between integer and string
-let c = 12;
-let d = "13";
-console.log(c + d); //concatinate
-
-let e = 12;
-let f = "13";
-console.log(e - f); //here concatinate does not work between string and number and if we use minus(-) sign javascript automatically convert string into number
-
-//sum and message
-let g = 15;
-let h = 15;
-console.log(g+h + " is sum of 15 and 15  " );
-
-//accept and print the number
-let age = prompt("Enter your age");
-console.log(age);
+let temp;
+temp = a;
+a = b;
+b = temp;
+console.log("a=", a);
+console.log("b=", b);
