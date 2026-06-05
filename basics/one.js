483eb2565d762fae84d753556b7502b826ed78a8
sagar joshi <sagarjo637@gmail.com>
2026-06-05 15:16:41 +0545

    practiced swapping variable using two method

--- basics/one.js
+++ basics/one.js
@@ -25,11 +25,26 @@
 //swap two variable via 3 method
 
 //first method
+// let a = 10;
+// let b = 20;
+// let temp;
+// temp = a;
+// a = b;
+// b = temp;
+// console.log("a=", a);
+// console.log("b=", b);
+
+//second method
+// let a = 10;
+// let b = 20;
+// a = a + b; //10+20=30
+// b = a - b; //30-20=10
+// a = a - b; //30-10=20
+// console.log(a);
+// console.log(b);
+
+//third method
 let a = 10;
 let b = 20;
-let temp;
-temp = a;
-a = b;
-b = temp;
-console.log("a=", a);
-console.log("b=", b);
+[a, b] = [b, a];
+console.log(a,b);
