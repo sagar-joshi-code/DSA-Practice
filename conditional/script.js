54345c389428cddae006c90a35f6edf1668cd9f3
sagar joshi <sagarjo637@gmail.com>
2026-06-08 19:14:08 +0545

    practice conditional question

--- conditional/script.js
+++ conditional/script.js
@@ -1,1 +1,9 @@
-
+//Valid voter
+let ans = Number(prompt("Enter your age"));
+if (isNaN(ans)) {
+  console.log("wrong input");
+} else if (ans >= 18) {
+  console.log("You are eligible for vote");
+} else {
+  console.log("You are not eligible for vote");
+}
