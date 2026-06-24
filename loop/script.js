712292c2d214a029fd9f2e791e1a1c9ca91b0d9f
sagar joshi <sagarjo637@gmail.com>
2026-06-24 16:43:52 +0545

    practiced looop question

--- loop/script.js
+++ loop/script.js
@@ -65,30 +65,62 @@
 
 //prime number
 
-let input = prompt("Enter a number");
-if (input === null) {
-  console.log("Cancelled");
-} else {
-  let num = Number(input);
-  if (isNaN(num)) {
-    console.log("Invalid input");
-  } else if (num < 0) {
-    console.log("Negative number");
-  } else if (num === 1) {
-    console.log("1 is not a prime  number");
-  } else if (num === 0) {
-    console.log("Number is zero");
-  } else {
-    let isPrime = true;
-    for (let i = 2; i <= Math.floor(num/2); i++){
-      if (num % i === 0) {
-        isPrime = false;
-        break;
-      }
-      
-    }
-    console.log(isPrime);
-    
-  }
+// let input = prompt("Enter a number");
+// if (input === null) {
+//   console.log("Cancelled");
+// } else {
+//   let num = Number(input);
+//   if (isNaN(num)) {
+//     console.log("Invalid input");
+//   } else if (num < 0) {
+//     console.log("Negative number");
+//   } else if (num === 1) {
+//     console.log("1 is not a prime  number");
+//   } else if (num === 0) {
+//     console.log("Number is zero");
+//   } else {
+//     let isPrime = true;
+//     for (let i = 2; i <= Math.floor(num / 2); i++) {
+//       if (num % i === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     console.log(isPrime);
+//   }
+// }
+
+//while loop syntax
+
+// start;
+// while (end) {
+//   //code
+//   Change;
+// }
+
+// let ans = prompt("kuch bhi dedo(exit for close)");
+// while (ans !== "exit") {
+//   ans = prompt("kuch bhi dedo(exit for close)");
+// }
+
+//sum of digit using while
+// let num = Number(prompt("enter number"));
+// let sum = 0;
+// while (num > 0) {
+//   rem = Math.floor(num % 10);
+//   sum += rem;
+//   num = num / 10;
+// }
+// console.log(sum);
+
+//reverse number
+let n = Number(prompt("enter number"));
+let reversed = 0;
+while (n > 0) {
+  let lastdigit = n % 10;
+  reversed = reversed * 10 + lastdigit;
+  n = Math.floor(n / 10);
 }
+console.log(reversed);
+
 
